391182bd12844a44dc64b2af245c56822df76c11
Gonçalo Silva <gnorsilva@gmail.com>
2012-08-04 09:50:25 +0100

    Small refactor for loggedUser and mocking this method

--- src/main/java/org/craftedsw/trip/TripService.java
+++ src/main/java/org/craftedsw/trip/TripService.java
@@ -1,18 +1,18 @@
 package org.craftedsw.trip;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.craftedsw.exceptions.UserNotLoggedInException;
 import org.craftedsw.user.User;
 import org.craftedsw.user.UserSession;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class TripService {
 
 	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
 		List<Trip> tripList = new ArrayList<Trip>();
-		User loggedUser = UserSession.getInstance().getLoggedUser();
+		User loggedUser = getLoggedUser();
 		boolean isFriend = false;
 		if (loggedUser != null) {
 			for (User friend : user.getFriends()) {
@@ -29,4 +29,8 @@ public class TripService {
 			throw new UserNotLoggedInException();
 		}
 	}
+
+    protected User getLoggedUser() {
+        return UserSession.getInstance().getLoggedUser();
+    }
 }
--- src/test/java/org/craftedsw/trip/TripServiceTest.java
+++ src/test/java/org/craftedsw/trip/TripServiceTest.java
@@ -1,6 +1,23 @@
 package org.craftedsw.trip;
 
+import org.craftedsw.user.User;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.spy;
+
 public class TripServiceTest {
-	
-	
+
+    @Test
+    public void testName() throws Exception {
+        User bob = new User();
+        TripService tripService = spy(new TripService());
+        doReturn(bob).when(tripService).getLoggedUser();
+        List<Trip> trips = tripService.getTripsByUser(bob);
+        assertThat(trips.size(), is(0));
+    }
 }
